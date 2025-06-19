19229f1b99eb08a881d4d70147ef710fec0efd53
abul.bashar@SCI-BD-L21-9006 <abul.bashar@savethechildren.org>
2025-06-19 12:11:01 +0600

    Depenedency Injection Implemeneted

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿public interface ICustomerDataAcess
+{
+    string GetCustomerName(int id);
+}
+public class CustomerDataAccess : ICustomerDataAcess
+{
+    public CustomerDataAccess()
+    {
+        
+    }
+    public string GetCustomerName(int id)
+    {
+        Console.WriteLine("Dummy Customer Name");
+        return "";
+    }
+
+}
+public class CustomerBusinessLogic
+{
+    ICustomerDataAcess _customerDataAcess;
+    public CustomerBusinessLogic(ICustomerDataAcess customerDataAcess)
+    {
+        _customerDataAcess = customerDataAcess;
+    }
+  
+    public string ProcessCustomerData(int id)
+    {
+        return _customerDataAcess.GetCustomerName(id);
+    }
+}
+public class CustomerService
+{
+    CustomerBusinessLogic _customerBusinessLogic;
+    public CustomerService()
+    {
+        _customerBusinessLogic = new CustomerBusinessLogic( new CustomerDataAccess());
+    }
+    public string GetCustomerName(int id)
+    {
+        return _customerBusinessLogic.ProcessCustomerData(id);
+
+    }
+}
+public class Program
+{ 
+    static void Main(string[] args)
+    {
+        CustomerService customerService = new CustomerService();
+        customerService.GetCustomerName(5);
+    }
+}
+
+
